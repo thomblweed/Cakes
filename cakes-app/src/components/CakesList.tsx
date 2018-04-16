1b45ecc3a223b575c7729ee0b7e094ebcbf5e571
Thomas Newman <thomas.newman@company-net.com>
2018-04-16 22:22:21 +0100

    Getting the Image and Name to show.

--- cakes-app/src/components/CakesList.tsx
+++ cakes-app/src/components/CakesList.tsx
@@ -9,9 +9,28 @@ export default class CakesList extends React.Component<CakesListProps> {
       return (
         <FocusZone>
           <List
-            items={this.props.cakes}            
+            items={this.props.cakes}
+            onRenderCell={this.onRenderCell}          
           />
         </FocusZone>
+      );
+    }
+
+    private onRenderCell(item: any, index: number | undefined): JSX.Element {
+      return (
+        <div
+          className="ms-ListGridExample-tile"
+          data-is-focusable={true}          
+        >
+          <div className="ms-ListGridExample-sizer">
+            <div className="msListGridExample-padder">
+              <img src={item.imageUrl} className="ms-ListGridExample-image" />
+              <span className="ms-ListGridExample-label">
+                {item.name}
+              </span>
+            </div>
+          </div>
+        </div>
       );
     }
   }
